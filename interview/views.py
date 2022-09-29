292fd5b0996b99f70fccdd982896c9896a32da5c
bakytaev <bakytaev@gmail.com>
2022-09-29 19:31:05 +0600

    make it better 2

--- interview/views.py
+++ interview/views.py
@@ -9,10 +9,6 @@ class CategoryListCreateAPIView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    def get_queryset(self):
-        print(self.kwargs)
-        return self.queryset.filter(category_id=self.kwargs['category_id'])
-
 
 class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
